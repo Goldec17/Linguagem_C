36df48f21023b9875be1c1a4cbcfdc675064b924
Goldec17 <134340829+Goldec17@users.noreply.github.com>
2025-09-30 17:28:45 -0300

    Create aula2.c

---
+++ aula2.c
@@ -0,0 +1,63 @@
+/*
+ * Então, vamos lá!!!
+ *
+ * As linguagens de programação são importantes para refazer processos rapidamente de forma automatizada,
+ * em todas as linguagens existem conceitos como:
+ * - Variaveis
+ * - Constantes
+ * - Estruturas de decisão
+ * - Laços de repetição
+ * - Estrutura de dados
+ * - Funções
+ *
+ * Como já vimos de variaveis, na questão de nome e idade, veremos agora sobre constantes!
+ * um exemplo de como criar uma constante: const tipo_const nome_const = valor_const;
+ * const float pi = 3.14; // Definindo o valor de π
+ *
+ * Então, assim como dados que não mudam, tipo:
+ * - data de nascimento
+ * - CPF
+ * - RG
+ *
+ * Você também deve aprender a usar as constantes no seu dia a dia.
+ *
+ * Como exercício, crie 10 variaveis e 10 constantes e atribua seus valores.
+ *
+ * ------------------------------------------------------------------------------------------ *
+ *
+ * Sobre as estruturas de decisão, inicialmente veremos os comandos IF e ELSE, veremos como,
+ * criar condições sobre dados.
+ *
+ * Imagine que você tem R$ 100.00, para comprar uma roupa de R$ 105.00, em um sistema as
+ * condições irão verificar se o seu saldo é maior ou igual o valor da peça de roupa. Se for,
+ * você compra!
+ * Você pode atribuir também ao sistema, uma ideia de pechincha!
+ * Se o saldo for menor que a peça de roupa, porém for maior que 90% do valor, você poderá
+ * comprar com desconto.
+ * Caso o saldo não seja nem igual ou maior que a roupa, nem maior que 90%, você não compra!
+ *
+ * Este conceito mostra condições, que em código em C, seriam:
+ *
+ * int saldo_cliente = 100, valor_peca_roupa = 105; 
+ * if (saldo_cliente >= valor_peca_roupa) {
+ *     // Você pode comprar a peça de roupa!
+ * }
+ * else if ((saldo_cliente < valor_peca_roupa) && saldo_cliente > ((valor_peca_roupa / 100) * 90)) {
+ *     // Você pode comprar a peça de roupa com desconto!
+ * }
+ * else {
+ *     // Você não pode comprar a peça de roupa!
+ * }
+ *
+ * Como exercício de condições, crie um programa que pergunte ao usuário, quantos anos ele tem, se
+ * for maior que 18 anos, deverá informar que ele é maior de idade e mostrar também sua idade.
+ * Se for menor, deverá informar para ele que ele é menor de idade e mostrar também sua idade.
+ *
+ * Por hoje, ficaremos com estes desafios!
+ * Ficamos para a próxima:
+ * Laços de repetição
+ * - Estrutura de dados
+ * - Funções
+ *
+ * Até a próxima!
+*/
